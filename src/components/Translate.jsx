7fb04fb10107f82906a0839958fbaea347f3f1aa
= <=>
2025-06-20 06:12:23 +0300

    Activate translate words in translate file

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,6 +1,8 @@
 
 import { useSelector, useDispatch } from "react-redux";
 
+import { useState, useEffect } from "react";
+
 import AuthService from "../service/auth-service";
 
 import logo from "../assets/logo.svg";
@@ -17,6 +19,56 @@ function Translate({ selectedWord = "", setShowAuth, show_auth }) {
   const dispatch = useDispatch();
 
   const is_auth = useSelector((state) => state.authSlice.is_auth);
+
+
+
+  const [fromLang, setFromLang] = useState("auto");
+  const [toLang, setToLang] = useState("en");
+  const [translation, setTranslation] = useState("");
+
+  const translate = async () => {
+    if (!selectedWord) return;
+
+    try {
+      // const response = await fetch(`https://api.libretranslate.com/translate`,  {
+      //   method: "POST",
+      //   body: JSON.stringify({
+      //     q: selectedWord,
+      //     source: fromLang === "auto" ? "auto" : "en",
+      //     target: toLang,
+      //   }),
+      //   headers: { "Content-Type": "application/json" }
+      // });
+
+      const res = await fetch("https://libretranslate.com/translate", {
+        method: "POST",
+        body: JSON.stringify({
+          q: "hello",
+          source: "auto",
+          target: "ru",
+          format: "text",
+          alternatives: 3,
+          api_key: ""
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+
+      const data = await res.json();
+      console.log('coming data is {}', data);
+      setTranslation(data.translations[0].text);
+    } catch (err) {
+      console.error("Translation failed:", err);
+      setTranslation("❌ Translation failed.");
+    }
+  };
+
+  useEffect(() => {
+    if (selectedWord) {
+      translate();
+    }
+  }, [selectedWord]);
+
+
 
   return (
     <div className="flex flex-col p-2 w-[30rem]">
@@ -81,19 +133,19 @@ function Translate({ selectedWord = "", setShowAuth, show_auth }) {
           id=""
           rows="8"
           placeholder="Translation will appear here..."
-          value={"This will be translation"}
+          value={translation || "Translating..."}
           readOnly
         ></textarea>
       </div>
 
-          {
-            is_auth &&
-            <div className="flex flex-row w-full mt-2 items-center justify-center">
-              <button className="py-2 text-sm w-full text-white bg-blue-800 rounded-sm cursor-pointer hover:bg-blue-500 duration-150">
-                Save Duo
-              </button>
-            </div>
-          }
+      {
+        is_auth &&
+        <div className="flex flex-row w-full mt-2 items-center justify-center">
+          <button className="py-2 text-sm w-full text-white bg-blue-800 rounded-sm cursor-pointer hover:bg-blue-500 duration-150">
+            Save Duo
+          </button>
+        </div>
+      }
 
     </div>
   );
